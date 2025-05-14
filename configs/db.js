ac151e7c4272a8d82e3d3176c53c1bfdb350ccf3
warakornkn98 <warakornkn98@gmail.com>
2025-05-14 15:51:23 +0700

    env

--- configs/db.js
+++ configs/db.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const sequelize = new Sequelize('mydb', 'user', 'userpass', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.MY_DB, process.env.MY_USER, process.env.MY_PASSWORD, {
+  host: process.env.MY_HOST,
   dialect: 'mysql',
 });
 
