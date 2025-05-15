ffa0d3f55aaf6ebf96babd9d8eef26d33c18fb75
TouchKittithat <kittithat.chubuathong@gmail.com>
2025-05-15 21:49:09 +0700

    add calPercent

--- routes/empolyee.js
+++ routes/empolyee.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const { createEmployee, getEmployeeById, getEmployeesByPagination, updateEmployee, deleteEmployee } = require('../controllers/employee.js');
+const { createEmployee, getEmployeeById, getEmployeesByPagination, updateEmployee, deleteEmployee, getProjectEmployeePercentage, getTrendEmployeePercentage } = require('../controllers/employee.js');
 
 router.get('/', getEmployeesByPagination);
+router.get('/percent_project', getProjectEmployeePercentage);
+router.get('/percent_trend', getTrendEmployeePercentage);
 router.post('/', createEmployee);
 router.get('/:id', getEmployeeById);
 router.put('/:id', updateEmployee);
